756f9bc32c40473dec24ad5e651fef214315794a
Jia Cui <cuijiajanae@gmail.com>
2021-06-18 09:29:58 -0700

    finished Authenticate Me part2

---
+++ frontend/src/index copy.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+
+// import './index.css';
+
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
+
+// import { restoreCSRF, csrfFetch } from './store/csrf';
+
+// import configureStore from './store';
+
+// import * as sessionActions from './store/session';
+
+// const store = configureStore();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   restoreCSRF();
+
+//   window.csrfFetch = csrfFetch;
+//   window.store = store;
+//   window.sessionActions = sessionActions;
+// }
+
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
+// function Root() {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
