5123e4317d87a7175e75705c6991a77592be5c2f
Jia Cui <cuijiajanae@gmail.com>
2021-06-19 10:47:31 -0700

    finished navEventDropDown

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 // frontend/src/components/Navigation/index.js
 
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
@@ -10,7 +10,7 @@ import logo from "../../images/logo.png"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-
+  const [showEventsMenu, setShowEventsMenu] = useState(false);
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
@@ -25,6 +25,24 @@ function Navigation({ isLoaded }){
     );
   }
 
+
+
+  const openEventsMenu = () => {
+    if (showEventsMenu) return;
+    setShowEventsMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showEventsMenu) return;
+
+    const closeMenu = () => {
+      setShowEventsMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showEventsMenu]);
   return (
     <>
       <div className = "navBar">
@@ -33,10 +51,21 @@ function Navigation({ isLoaded }){
 
           <div className="navLinks">
             <NavLink className ="navHomeLink" exact to="/">HOME</NavLink>
-            <NavLink className ="navEventsLink" exact to="/">EVENTS</NavLink>
+            <NavLink onClick={openEventsMenu} className ="navEventsLink" exact to="/">EVENTS</NavLink>
             <NavLink className ="navTicketsLink" exact to="/">TICKETS</NavLink>
             <NavLink className ="navMyEventsLink" exact to="/">My EVENTS</NavLink>
             <NavLink className ="navBookmarksLink" exact to="/">BOOKMARKS</NavLink>
+          </div>
+          <div className="navEventsLinkDropDown">
+            {showEventsMenu && (
+              <ul className="dropdown-events-menu">
+                <div className="dropdown-events-menu-genres" ><a>GENRES</a></div>
+                <div className="dropdown-events-menu-date" ><a>DATE</a></div>
+                <div>
+                  <a className="dropdown-events-menu-places" >PLACES</a>
+                </div>
+              </ul>
+            )}
           </div>
           <div className="logoAndAuth">
 
