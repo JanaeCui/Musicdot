c56280e8916634e0a47bb947acca93fa05197db4
Jia Cui <cuijiajanae@gmail.com>
2021-06-19 10:07:32 -0700

    finished footer without hover

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./Footer.css"
+import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 function Footer() {
     return(
         <div className= "footer">
-
+            <div className="footerWebName">MUSIC DOT</div>
+            <div className="footerIcons">
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-vimeo"></i>
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-youtube"></i>
+                <i class="fab fa-snapchat-ghost"></i>
+            </div>
+            <div className="footerCopyRight">© 2021 music dot copy right</div>
         </div>
     )
 }
