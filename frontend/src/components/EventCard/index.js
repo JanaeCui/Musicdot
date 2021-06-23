12c31fddcce3340c91ff51059b1453e01c46f1d3
Jia Cui <cuijiajanae@gmail.com>
2021-06-22 19:53:04 -0700

    finished getEventsByGenre

--- frontend/src/components/EventCard/index.js
+++ frontend/src/components/EventCard/index.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import "./EventCard.css"
 import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import {Redirect} from "react-router-dom";
-
-import { getEvents } from '../../store/events';
 
 import { format } from 'date-fns'
 import { enGB } from 'date-fns/locale'
 
 
 function EventCard({event}) {
+    const [contentCard, setContentCard] = useState("")
 
-    // const dispatch = useDispatch();
-    // const events = useSelector((state) => Object.values(state.events));
+    const [price, setPrice] = useState("")
+
+    useEffect(()=>{
+        if(event.id % 2 === 0){
+            console.log(event.id);
+            setContentCard("contentCard2")
+        }else{
+            setContentCard("contentCard1")
+        }
+    },[])
+
+    useEffect(()=>{
+        if(event.id % 2 === 0){
+            console.log(event.id);
+            setPrice("price2")
+        }else{
+            setPrice("price1")
+        }
+    },[])
+
     const sessionUser = useSelector((state)=> state.session.user);
-
-    // useEffect(() => {
-    //     if(sessionUser){
-    //         dispatch(getEvents());
-    //     }
-    //   }, [dispatch]);
 
     if(!sessionUser){
         return <Redirect to="/signup" />
@@ -30,14 +40,13 @@ function EventCard({event}) {
 
     return(
         <>
-        {/* {events.map((event) =>{ */}
-            return <div className="eventCard">
+            <div className="eventCard">
                 <img className= "eventCardPic" src={event.Image.eventImageUrl} alt="eventCardPic" />
-                <div className="contentCard1">
+                <div className={contentCard}>
                     <div className="content">
                         <div className="title">{event.title}</div>
                         <div className="date">{format(new Date(event.date), 'dd MMM yyyy', { locale: enGB })}</div>
-                        <div className="price1">Starts at ${event.price}</div>
+                        <div className={price}>Starts at ${event.price}</div>
                         <div className="capacity">Capacity: {event.capacity} people</div>
                         <div className="iconsGroup" style={{display:"display"}}>
                             <i className="far fa-bookmark" style={{display:"display"}}></i>
@@ -46,30 +55,8 @@ function EventCard({event}) {
                     </div>
                 </div>
             </div>
-        {/* } */}
-        )
-        {/* } */}
         </>
     )
-
 }
 
 export default EventCard;
-
-
-
-{/* <div className="eventCard">
-    <img className= "eventCardPic" src="/images/eventCardPic1.png" alt="eventCardPic" />
-    <div className="contentCard1">
-        <div className="content">
-            <div className="title">COACHELLA</div>
-            <div className="date">Thu, Jul 15, 2021 10:30 AM PDT</div>
-            <div className="price1">Starts at $3.00</div>
-            <div className="capacity">Capacity: 25 people</div>
-            <div className="iconsGroup" style={{display:"display"}}>
-                <i className="far fa-bookmark" style={{display:"display"}}></i>
-                <i className="fas fa-cart-plus" style={{display:"display"}}></i>
-            </div>
-        </div>
-    </div>
-</div> */}
--- frontend/src/components/EventGenrePage/index.js
+++ frontend/src/components/EventGenrePage/index.js
@@ -5,7 +5,7 @@ import EventCard from '../EventCard';
 import CD from '../../images/CD.png'
 import SearchBar from "../../components/SearchBar"
 
-import { useEffect } from 'react';
+import { useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {Redirect} from "react-router-dom";
 
@@ -16,6 +16,7 @@ function EventGenrePage() {
     const dispatch = useDispatch();
     const events = useSelector((state) => Object.values(state.events));
     const sessionUser = useSelector((state)=> state.session.user);
+    const categories = events.map(event => event.category);
 
     useEffect(() => {
         if(sessionUser){
@@ -27,90 +28,56 @@ function EventGenrePage() {
         return <Redirect to="/signup" />
     }
 
-    const eventGenrePopGroup =()=>{
-        events.map((event) =>{if(event.category === "pop"){
-            return <div className="eventGenreOuterDiv">
-                        <img className= "eventsCD" src={CD} alt="CD"/>
-                        <div className="eventTitleAndSearchBar">
-                            <div className="eventGenreTitle">
-                                LIST BY GENRES
-                            </div>
-                            <SearchBar className="searchBarInEventsPage"/>
-                        </div>
-                        <div className="eventGenre">{event.category}</div>
-                        <EventCard event={event} className="eventCard" />
-                    </div>
-        }})
-    }
 
+
+    const eventGenreGroup = (genre)=>{
+
+            return events.map((event) =>{
+
+                if(event.category === genre){
+                 return  <>
+                             <EventCard event={event} className="eventCard" />
+                         </>
+
+                 }
+                 else{
+                     return null;
+                 }
+
+             })
+
+    }
 
     return(
         <>
-            {eventGenrePopGroup()}
+             <div className="eventGenreOuterDiv">
+                <img className= "eventsCD" src={CD} alt="CD"/>
+                <div className="eventTitleAndSearchBar">
+                    <div className="eventGenreTitle">
+                        LIST BY GENRES
+                    </div>
+                    <SearchBar className="searchBarInEventsPage"/>
+                </div>
+                {categories.includes("pop") && <div className="eventGenre">POP</div>}
+                {eventGenreGroup("pop")}
+                {categories.includes("rock") && <div className="eventGenre">ROCK</div>}
+                {eventGenreGroup("rock")}
+                {categories.includes("country") && <div className="eventGenre">COUNTRY</div>}
+                {eventGenreGroup("country")}
+                {categories.includes("hip hop") && <div className="eventGenre">HIP POP</div>}
+                {eventGenreGroup("hip hop")}
+                {categories.includes("jazz") && <div className="eventGenre">JAZZ</div>}
+                {eventGenreGroup("jazz")}
+                {categories.includes("funk") && <div className="eventGenre">FUNK</div>}
+                {eventGenreGroup("funk")}
+                {categories.includes("blues") && <div className="eventGenre">BLUES</div>}
+                {eventGenreGroup("blues")}
+            </div>
+
         </>
-
-        // <div className="eventGenreOuterDiv">
-        //     <img className= "eventsCD" src={CD} alt="CD"/>
-        //     <div className="eventTitleAndSearchBar">
-        //         <div className="eventGenreTitle">
-        //             LIST BY GENRES
-        //         </div>
-        //         <SearchBar className="searchBarInEventsPage"/>
-        //     </div>
-        //     <div className="eventGenre">{event.category}</div>
-        //     <EventCard className="eventCard" />
-
-        //     <div className="eventGenre">ROCK</div>
-        //     <EventCard className="eventCard"/>
-
-        //     <div className="eventGenre">COUNTRY</div>
-        //     <EventCard className="eventCard"/>
-
-        //     <div className="eventGenre">HIP HOP</div>
-        //     <EventCard className="eventCard"/>
-
-        //     <div className="eventGenre">JAZZ</div>
-        //     <EventCard className="eventCard"/>
-
-        //     <div className="eventGenre">FUNK</div>
-        //     <EventCard className="eventCard"/>
-
-        //     <div className="eventGenre">BLUES</div>
-        //     <EventCard className="eventCard"/>
-        // </div>
 
     )
 
 }
 
 export default EventGenrePage;
-
-{/* <div className="eventGenreOuterDiv">
-    <img className= "eventsCD" src={CD} alt="CD"/>
-    <div className="eventTitleAndSearchBar">
-        <div className="eventGenreTitle">
-            LIST BY GENRES
-        </div>
-        <SearchBar className="searchBarInEventsPage"/>
-    </div>
-    <div className="eventGenre">{event.category}</div>
-    <EventCard className="eventCard" />
-
-    <div className="eventGenre">ROCK</div>
-    <EventCard className="eventCard"/>
-
-    <div className="eventGenre">COUNTRY</div>
-    <EventCard className="eventCard"/>
-
-    <div className="eventGenre">HIP HOP</div>
-    <EventCard className="eventCard"/>
-
-    <div className="eventGenre">JAZZ</div>
-    <EventCard className="eventCard"/>
-
-    <div className="eventGenre">FUNK</div>
-    <EventCard className="eventCard"/>
-
-    <div className="eventGenre">BLUES</div>
-    <EventCard className="eventCard"/>
-</div> */}
