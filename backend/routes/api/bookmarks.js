c175126149e082f4859454e21191248f9b1eb3c0
Jia Cui <cuijiajanae@gmail.com>
2021-06-24 17:04:23 -0700

    can work with the empty and solid bookmark

--- backend/routes/api/bookmarks.js
+++ backend/routes/api/bookmarks.js
@@ -26,7 +26,29 @@ router.post('/',requireAuth, asyncHandler(async function(req, res) {
 
      return res.json(bookmark)
     // res.redirect(`/bookmarks`);
-})
+    })
 );
+
+
+router.post('/isBookmarked', requireAuth, asyncHandler(async function(req, res) {
+
+    const userId = req.body.userId;
+    const eventId = req.body.eventId;
+    const bookmark = await Bookmark.findOne({
+        where: {userId, eventId}
+    });
+    let isBookmarked;
+    if(bookmark){
+        isBookmarked = true;
+    }else{
+        isBookmarked = false;
+    }
+
+    return res.json(isBookmarked);
+   // res.redirect(`/bookmarks`);
+   })
+
+)
+
 
 module.exports = router;
