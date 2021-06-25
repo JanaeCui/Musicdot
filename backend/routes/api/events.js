3b96660ff98c82bb7ef6e77c986aec4a87d5293e
Jia Cui <cuijiajanae@gmail.com>
2021-06-24 23:30:12 -0700

    clickBookmark can work but need refresh

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -33,7 +33,7 @@ router.get('/',requireAuth, asyncHandler(async (req, res) => {
       }
     })
 
-    // if(!currentBookmark) {
+    if(!currentBookmark) {
         const newBookmark = await Bookmark.build({
             userId: loggedUserId,
             eventId
@@ -42,8 +42,46 @@ router.get('/',requireAuth, asyncHandler(async (req, res) => {
         bookmarkState = true
         // res.locals.bookmarkId = true
         await newBookmark.save()
+    }
+
+    if (typeof window !== 'undefined') {
+        localStorage.setItem("savedBookmarkState", false);
+    }
+
+    res.json({currentBookmark, bookmarkState})
+})
+);
+
+router.delete('/:id/bookmarks',requireAuth, asyncHandler(async function(req, res) {
+
+    const eventId = parseInt(req.params.id, 10);
+    // const userId = req.body.userId;
+    // console.log("--------",loggedUserId);
+    let bookmarkState = false
+
+
+    // if (typeof window !== 'undefined') {
+    //     localStorage.setItem("savedBookmarkState", true);
+    // }
+    const currentBookmark = await Bookmark.findOne({
+      where: {
+          eventId,
+        //   userId: userId
+      }
+    })
+
+    // if(!currentBookmark) {
+        const newBookmark = await Bookmark.destroy({
+            where: { eventId}
+        })
+        // const newBookmark = await Bookmark.create(req.body);
+        bookmarkState = false
+        // res.locals.bookmarkId = true
     // }
 
+    if (typeof window !== 'undefined') {
+        localStorage.setItem("savedBookmarkState", true);
+    }
 
     res.json({currentBookmark, bookmarkState})
 })
