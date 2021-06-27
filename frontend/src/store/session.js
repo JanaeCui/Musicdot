8fa3f8a45bbda8040366353a3fa3aef3df1207ce
Jia Cui <cuijiajanae@gmail.com>
2021-06-27 13:47:51 -0700

    add cosle.log

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -19,6 +19,8 @@ const removeUser = () => {
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
+  console.log("credential: " + credential)
+  console.log("password: " + password)
   const response = await csrfFetch('/api/session', {
     method: 'POST',
     body: JSON.stringify({
